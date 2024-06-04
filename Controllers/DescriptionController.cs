e1c68ffb6b845ed0d390eac47e00910a38594b15
gilsonconceicao <gilsonsantosjunior02@gmail.com>
2024-06-03 21:59:57 -0300

    add swagger and docuementation

--- Controllers/DescriptionController.cs
+++ Controllers/DescriptionController.cs
@@ -2,18 +2,35 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using Encryption_and_decryption_csharp.services.Commands;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Encryption_and_decryption_csharp.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class DescriptionController: ControllerBase
+    public class DescriptionController : ControllerBase
     {
-        [HttpGet]
-        public async Task<IActionResult> GetDescriptions() 
+        private readonly IMediator _mediator;
+
+        public DescriptionController(IMediator mediator)
         {
-            return Ok(); 
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// Realiza a criptografia de um texto 
+        /// </summary>
+        /// <returns>Criptografia</returns>
+        /// <response code="200">200 Sucesso</response>
+        /// <response code="400">400 Erro</response>
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(EncryptAndDecryptResult))]
+        [HttpPost("Encript")]
+        public async Task<IActionResult> Encrypt([FromBody] EncryptAndDecryptCommand values)
+        {
+            var result = await _mediator.Send(values);
+            return Ok(result);
         }
     }
 }
